4f1d57a8e41579bdf8f316aaea32ca760b4804c4
Dorota Zelga <dorota.zelga@gmail.com>
2020-03-14 13:52:27 +0100

    ShoppingList.js component added

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
 import './App.css';
 import TopBar from "./TopBar";
+import ShoppingList from "./ShoppingList";
 import BottomNav from "./BottomNav";
 
 function App() {
     return (
         <div>
             <TopBar/>
+            <BrowserRouter>
+                <div>
+                    <Route path="/" exact component={ShoppingList} />
+                </div>
+            </BrowserRouter>
             <BottomNav/>
         </div>
     );
---
+++ src/components/ShoppingList.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function ShoppingList() {
+    return (
+        <div>Shopping List</div>
+    )
+}
+
+export default ShoppingList
