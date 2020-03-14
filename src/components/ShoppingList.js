cb011192f241f686ec03486964cc61fd5af09df8
Dorota Zelga <dorota.zelga@gmail.com>
2020-03-14 14:56:33 +0100

    shoppingList > render options

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {connect} from 'react-redux';
 import {createStyles, makeStyles} from '@material-ui/core/styles';
 import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
@@ -13,19 +14,29 @@ const useStyles = makeStyles((theme) =>
     }),
 );
 
-const ShoppingList = () => {
+const ShoppingList = (props) => {
     const classes = useStyles();
     const [category, setCategory] = React.useState('');
+
+    const renderOptions = () => {
+        return props.categories.map(category => {
+            return (
+                <MenuItem value={category.id}>{category.label}</MenuItem>
+            )
+        })
+    };
 
     return (
         <div>
             <FormControl className={classes.formControl}>
-                <Select value={category}>
-                    <MenuItem value={1}>Category</MenuItem>
-                </Select>
+                <Select value={category}>{renderOptions()}</Select>
             </FormControl>
         </div>
     )
 };
 
-export default ShoppingList
+const mapStateToProps = state => {
+    return {categories: state.categories};
+};
+
+export default connect(mapStateToProps)(ShoppingList);
