ed52488efc6fbd348a53ef6a196eef25eaa60e4d
Dorota Zelga <dorota.zelga@gmail.com>
2020-03-14 14:11:04 +0100

    ShoppingList.js component - draft of category selector

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,8 +1,30 @@
 import React from "react";
+import {createStyles, makeStyles} from '@material-ui/core/styles';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+
+const useStyles = makeStyles((theme) =>
+    createStyles({
+        formControl: {
+            margin: theme.spacing(1),
+            minWidth: 120,
+        },
+    }),
+);
 
 function ShoppingList() {
+    const classes = useStyles();
+    const [category, setCategory] = React.useState('');
+
     return (
-        <div>Shopping List</div>
+        <div>
+            <FormControl className={classes.formControl}>
+                <Select value={category}>
+                    <MenuItem value={1}>Category</MenuItem>
+                </Select>
+            </FormControl>
+        </div>
     )
 }
 
