92e22e2bdd4439a72f33750fe35bf644e58fac38
Dorota Zelga <dorota.zelga@gmail.com>
2020-03-14 15:33:25 +0100

    select category moved to CategorySelector.js component

---
+++ src/components/CategorySelector.js
@@ -0,0 +1,51 @@
+import React from "react";
+import {connect} from 'react-redux';
+import {createStyles, makeStyles} from '@material-ui/core/styles';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+
+import {selectCategory} from "../actions";
+
+const useStyles = makeStyles((theme) =>
+    createStyles({
+        formControl: {
+            margin: theme.spacing(1),
+            minWidth: 120,
+        },
+    }),
+);
+
+const CategorySelector = (props) => {
+    const classes = useStyles();
+
+    const handleChange = event => {
+        props.selectCategory(event.target.value)
+    };
+
+    const renderOptions = () => {
+        return props.categories.map(category => {
+            return (
+                <MenuItem value={category.id} key={category.id}>{category.label}</MenuItem>
+            )
+        })
+    };
+
+    return (
+        <div>
+            <FormControl className={classes.formControl}>
+                <Select value={props.selectedCategory} onChange={handleChange}>{renderOptions()}</Select>
+            </FormControl>
+        </div>
+    )
+};
+
+const mapStateToProps = state => {
+    return {
+        categories: state.categories,
+        selectCategory: state.selectCategory,
+        selectedCategory: state.selectedCategory || state.categories[0].id
+    };
+};
+
+export default connect(mapStateToProps, {selectCategory})(CategorySelector);
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,51 +1,13 @@
 import React from "react";
-import {connect} from 'react-redux';
-import {createStyles, makeStyles} from '@material-ui/core/styles';
-import MenuItem from '@material-ui/core/MenuItem';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
+import CategorySelector from "./CategorySelector";
 
-import {selectCategory} from "../actions";
-
-const useStyles = makeStyles((theme) =>
-    createStyles({
-        formControl: {
-            margin: theme.spacing(1),
-            minWidth: 120,
-        },
-    }),
-);
-
-const ShoppingList = (props) => {
-    const classes = useStyles();
-
-    const handleChange = event => {
-        props.selectCategory(event.target.value)
-    };
-
-    const renderOptions = () => {
-        return props.categories.map(category => {
-            return (
-                <MenuItem value={category.id} key={category.id}>{category.label}</MenuItem>
-            )
-        })
-    };
-
+const ShoppingList = () => {
     return (
         <div>
-            <FormControl className={classes.formControl}>
-                <Select value={props.selectedCategory} onChange={handleChange}>{renderOptions()}</Select>
-            </FormControl>
+            <CategorySelector/>
         </div>
     )
 };
 
-const mapStateToProps = state => {
-    return {
-        categories: state.categories,
-        selectCategory: state.selectCategory,
-        selectedCategory: state.selectedCategory || state.categories[0].id
-    };
-};
 
-export default connect(mapStateToProps, {selectCategory})(ShoppingList);
+export default ShoppingList;
