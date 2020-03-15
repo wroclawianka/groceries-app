13220374541d1f91beabc841186892e40527a5fc
Dorota Zelga <dorota.zelga@gmail.com>
2020-03-15 18:09:54 +0100

    edit item reducer added

--- frontend/src/reducers/itemsReducer.js
+++ frontend/src/reducers/itemsReducer.js
@@ -1,10 +1,16 @@
-import {CREATE_ITEM, FETCH_ITEMS} from "../actions/types";
+import {
+    CREATE_ITEM,
+    EDIT_ITEM,
+    FETCH_ITEMS
+} from "../actions/types";
 
 export default (state = null, action) => {
     switch (action.type) {
         case FETCH_ITEMS:
             return {...state, items: action.payload};
         case CREATE_ITEM:
+            return {...state, item: action.payload};
+        case EDIT_ITEM:
             return {...state, item: action.payload};
         default:
             return state;
