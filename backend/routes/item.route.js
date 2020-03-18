6a3107c454fa732a4f92ed0f8878d9d7695583af
Dorota Zelga <dorota.zelga@gmail.com>
2020-03-18 09:42:56 +0100

    item routes > search by category only if specified

--- backend/routes/item.route.js
+++ backend/routes/item.route.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const Item = require('../models/item.model');
 
 router.get('/item', (req, res) => {
-    const category = req.query.value;
-    Item.find({category: category}, (err, data) => {
+    const category = req.query.category;
+    const params = (category) ? {category} : {};
+    Item.find(params, (err, data) => {
         if (err) return res.json({success: false, error: err});
         return res.json(data);
     });
