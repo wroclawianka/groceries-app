6f90971ff408044ef20cff8e3ffd3704b979ee60
Dorota Zelga <dorota.zelga@gmail.com>
2020-03-21 10:48:42 +0100

    Category reducer > fetch categories > add "All" to list of categories

--- frontend/src/reducers/categoryReducer.js
+++ frontend/src/reducers/categoryReducer.js
@@ -1,9 +1,12 @@
 import {FETCH_CATEGORIES, SELECT_CATEGORY} from "../actions/types";
+import _ from "lodash";
 
 export default (state = null, action) => {
     switch (action.type) {
         case FETCH_CATEGORIES:
-            return {...state, list: action.payload};
+            const allCategory = {_id: "0", label: "All"};
+            action.payload.push(allCategory);
+            return {...state, ..._.mapKeys(action.payload, "_id")};
         case SELECT_CATEGORY:
             return {...state, selected: action.payload};
         default:
