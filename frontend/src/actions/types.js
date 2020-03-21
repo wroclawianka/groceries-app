968452459beac3a10b5eab36a8e3f5354b20c2df
Dorota Zelga <dorota.zelga@gmail.com>
2020-03-21 18:16:18 +0100

    select item action removed

--- frontend/src/actions/types.js
+++ frontend/src/actions/types.js
@@ -4,5 +4,4 @@ export const FETCH_CATEGORIES = 'FETCH_CATEGORIES';
 export const SELECT_CATEGORY = 'SELECT_CATEGORY';
 export const FETCH_ITEMS = 'FETCH_ITEMS';
 export const CREATE_ITEM = 'CREATE_ITEM';
-export const SELECT_ITEM = 'SELECT_ITEM';
 export const EDIT_ITEM = 'EDIT_ITEM';
