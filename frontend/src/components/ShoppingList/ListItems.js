d870302b575934e06c0361c8bd1cabb7b561eba8
Dorota Zelga <dorota.zelga@gmail.com>
2020-03-21 10:52:42 +0100

    List items component > code refactor

--- frontend/src/components/ShoppingList/ListItems.js
+++ frontend/src/components/ShoppingList/ListItems.js
@@ -1,6 +1,4 @@
 import React from "react";
-import {connect} from 'react-redux';
-import _ from "lodash";
 import Checkbox from "@material-ui/core/Checkbox";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
