42ee52d93a0311c29d805b57ae65e6eeb1d93820
Dorota Zelga <dorota.zelga@gmail.com>
2020-03-21 12:05:19 +0100

    Shopping List > edit item

--- frontend/src/components/ShoppingList/ShoppingList.js
+++ frontend/src/components/ShoppingList/ShoppingList.js
@@ -6,7 +6,7 @@ import CategorySelector from "./CategorySelector";
 import ListItems from "./ListItems";
 import ListCompletedItems from "./ListCompletedItems";
 import classes from '../../styles/styles.module.css'
-import {fetchItems, createItem, selectCategory} from "../../actions";
+import {fetchItems, createItem, editItem, selectCategory} from "../../actions";
 
 class ShoppingList extends React.Component {
     constructor(props) {
@@ -49,15 +49,16 @@ class ShoppingList extends React.Component {
         this.props.createItem(item, this.state.selectedCategory)
     };
 
+    editItem = (item) => {
+        this.props.editItem(item)
+    };
+
     renderLists() {
         if (this.props.items) {
-            let items = Object.values(this.props.items);
-            items = _.groupBy(items, "completed");
-            const [toBought, completed] = [items.false, items.true];
             return (
                 <div>
-                    <ListItems items={toBought}/>
-                    <ListCompletedItems items={completed}/>
+                    <ListItems items={this.state.itemsToBought} editItem={this.editItem}/>
+                    <ListCompletedItems items={this.state.itemsCompleted}/>
                 </div>
             )
         }
@@ -87,5 +88,5 @@ const mapStateToProps = (state) => {
 
 export default connect(
     mapStateToProps,
-    {fetchItems, createItem, selectCategory}
+    {fetchItems, createItem, editItem, selectCategory}
 )(ShoppingList);
